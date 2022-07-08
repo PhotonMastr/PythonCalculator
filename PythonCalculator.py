3211eca9d5eebbb433545686a13932a737b54fac
t <87922964+PhotonMastr@users.noreply.github.com>
2022-07-08 00:17:58 -0400

    Fixed
    the invalid operator line didn't work now it does lol

--- PythonCalculator.py
+++ PythonCalculator.py
@@ -2,18 +2,18 @@ import os
 
 number1 = float(input("Enter first number: "))
 os.system('cls' if os.name == 'nt' else 'clear')
-operatorr = input("Enter Operator (you can also type multiply, or add for example): ")
+operatorr = input("Enter Operator (+, -, *, /): ")
 os.system('cls' if os.name == 'nt' else 'clear')
 number2 = float(input("Enter second number: "))
 os.system('cls' if os.name == 'nt' else 'clear')
 
-if operatorr == "+" or "add":
+if operatorr == "+":
     print(number1 + number2)
-elif operatorr == "-" or "minus":
+elif operatorr == "-":
     print(number1 - number2)
-elif operatorr == "/" or "divide": 
+elif operatorr == "/": 
     print(number1 / number2)
-elif operatorr == "*" or "multiply":
+elif operatorr == "*":
     print(number1 * number2)
 else:
-    print("That's not a valid operator!")
+    print("You have entered an invalid operator!")
