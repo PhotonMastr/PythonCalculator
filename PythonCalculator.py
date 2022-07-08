03d397b680313a26d503b7dedf4d3cd71876ab2d
t <87922964+PhotonMastr@users.noreply.github.com>
2022-07-08 00:07:17 -0400

    AYY
    my dumbass forgot that i can just make an if statement for clearing the screen if it's *NIX or windows lol

--- PythonCalculator.py
+++ PythonCalculator.py
@@ -1,11 +1,11 @@
-import os
+import os 
 
 number1 = float(input("Enter first number: "))
-os.system('clear')
+os.system('cls' if os.name == 'nt' else 'clear')
 operatorr = input("Enter Operator (you can also type multiply, or add for example): ")
-os.system('clear')
+os.system('cls' if os.name == 'nt' else 'clear')
 number2 = float(input("Enter second number: "))
-os.system('clear')
+os.system('cls' if os.name == 'nt' else 'clear')
 
 if operatorr == "+" or "add":
     print(number1 + number2)
